0fe27ac3ed1c5ef5339e25835e3f380c429c83c0
kmw0428 <anselmo12@naver.com>
2024-05-27 21:39:39 +0900

    테스트 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Test from './Test'
 import ProductList from './assets/Product/ProductList'
+import Diagnosis from './assets/Diagnosis/Diagonsis'
+import Result from './assets/Diagnosis/Result'
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Test />} />
         <Route path='/products' element={<ProductList />} />
+        <Route path='/diagnosis' element={<Diagnosis />} />
+        <Route path='/result' element={<Result />} />
       </Routes>
     </>
   )
