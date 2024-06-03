8ad22cf470def968d33f62cd86a0ef5b315b29b9
Kim MooWon <anselmo12@naver.com>
2024-06-03 15:43:11 +0900

    V 1.13

--- src/assets/Nav_Foot/Navbar.tsx
+++ src/assets/Nav_Foot/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
@@ -7,7 +7,6 @@ const Navbar: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
-  const lastScrollTop = useRef(0);
 
   const handleSearchClick = () => {
     setShowSearch(!showSearch);
@@ -15,14 +14,13 @@ const Navbar: React.FC = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop.current) {
-        setIsVisible(false);
-      } else {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop === 0) {
         setIsVisible(true);
+      } else {
+        setIsVisible(false);
       }
-      lastScrollTop.current = scrollTop <= 0 ? 0 : scrollTop;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -38,88 +36,88 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-    <nav className={isVisible ? "visible" : "hidden"}>
-      <div className="navbar__logo">
-        <li>
+      <nav className={isVisible ? "visible" : "hidden"}>
+        <div className="navbar__logo">
           <Link to="/">
-            <img src="/logo.png" alt="Logo" style={{ height: "40px" }} />
+            <li>
+              <img src="/logo.png" alt="Logo" style={{ height: "40px" }} />
+            </li>
           </Link>
-        </li>
-      </div>
-      <ul className="main-menu">
-        <li>
-          <span>진단</span>
-          <ul className="sub-menu">
-            <li>
-              <Link to="/diagnosisSkin">피부 진단</Link>
-            </li>
-            <li>
-              <Link to="/diagnosisSclap">두피 진단</Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/product-list">전체</Link>
-        </li>
-        <li>
-          <Link to="/custom">맞춤</Link>
-        </li>
-        <li>
-          <span>스킨</span>
-          <ul className="sub-menu">
-            <li>
-              <Link to="/skincare/sub1">클렌져</Link>
-            </li>
-            <li>
-              <Link to="/skincare/sub2">토너</Link>
-            </li>
-            <li>
-              <Link to="/skincare/sub3">세럼&에센스</Link>
-            </li>
-            <li>
-              <Link to="/skincare/sub2">로션&크림</Link>
-            </li>
-            <li>
-              <Link to="/skincare/sub2">선케어</Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <span>두피</span>
-          <ul className="sub-menu">
-            <li>
-              <Link to="/scalp/sub1">샴푸</Link>
-            </li>
-            <li>
-              <Link to="/scalp/sub2">트리트먼트</Link>
-            </li>
-            <li>
-              <Link to="/scalp/sub2">토닉&세럼</Link>
-            </li>
-            <li>
-              <Link to="/scalp/sub2">스케일링</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <ul className="login">
-        <li>
-          <img src="/search-icon.png" style={{height: "20px", paddingTop: "2px", cursor: "pointer"}} onClick={handleSearchClick} />
-        </li>
-        {isLoggedIn ? (
+        </div>
+        <ul className="main-menu">
           <li>
-            <Link to="/mypage">마이페이지</Link> /{" "}
-            <button onClick={handleLogout}>로그아웃</button>
+            <span>진단</span>
+            <ul className="sub-menu">
+              <Link to="/diagnosisSkin">
+                <li>피부 진단</li>
+              </Link>
+              <Link to="/diagnosisSclap">
+                <li>두피 진단</li>
+              </Link>
+            </ul>
           </li>
-        ) : (
+          <Link to="/product-list">
+            <li>전체</li>
+          </Link>
+          <Link to="/custom">
+            <li>맞춤</li>
+          </Link>
           <li>
-            <Link to="/login">로그인 / 회원가입</Link>
+            <span>스킨</span>
+            <ul className="sub-menu">
+              <Link to="/skincare/sub1">
+                <li>클렌져</li>
+              </Link>
+              <Link to="/skincare/sub2">
+                <li>토너</li>
+              </Link>
+              <Link to="/skincare/sub3">
+                <li>세럼&에센스</li>
+              </Link>
+              <Link to="/skincare/sub2">
+                <li>로션&크림</li>
+              </Link>
+              <Link to="/skincare/sub2">
+                <li>선케어</li>
+              </Link>
+            </ul>
           </li>
-        )}
-      </ul>
-    </nav>
+          <li>
+            <span>두피</span>
+            <ul className="sub-menu">
+              <Link to="/scalp/sub1">
+                <li>샴푸</li>
+              </Link>
+              <Link to="/scalp/sub2">
+                <li>트리트먼트</li>
+              </Link>
+              <Link to="/scalp/sub2">
+                <li>토닉&세럼</li>
+              </Link>
+              <Link to="/scalp/sub2">
+                <li>스케일링</li>
+              </Link>
+            </ul>
+          </li>
+        </ul>
+        <ul className="login">
+          <li>
+            <img src="/search-icon.png" style={{ height: "20px", paddingTop: "2px", cursor: "pointer" }} onClick={handleSearchClick} />
+          </li>
+          {isLoggedIn ? (
+            <li>
+              <Link to="/mypage">마이페이지</Link> /{" "}
+              <button onClick={handleLogout}>로그아웃</button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">로그인 / 회원가입</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
 
-{showSearch && <SearchBar />}</>
+      {showSearch && <SearchBar />}</>
   );
 };
 
