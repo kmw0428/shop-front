da13ed0890cafd6ad53004bae6365b1ed4100771
Jihyun O <wlgus0155@naver.com>
2024-06-07 17:18:17 +0900

    회원정보 수정 css 제작

--- src/assets/User/EditUser.tsx
+++ src/assets/User/EditUser.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../Auth/axiosInstance"; // axiosInstance 사용
+import "./EditUser.css";
 
 interface User {
   id: string;
@@ -25,14 +26,16 @@ const EditUser: React.FC = () => {
 
   useEffect(() => {
     const fetchUserData = async () => {
-        const userId = localStorage.getItem("userId"); // 로컬 스토리지에서 사용자 ID 가져오기
-        if (!userId) {
-          setError("User ID not found");
-          setLoading(false);
-          return;
-        }
+      const userId = localStorage.getItem("userId"); // 로컬 스토리지에서 사용자 ID 가져오기
+      if (!userId) {
+        setError("User ID not found");
+        setLoading(false);
+        return;
+      }
       try {
-        const response = await axiosInstance.get(`http://localhost:8080/api/users/${userId}`); // 현재 사용자 정보 엔드포인트
+        const response = await axiosInstance.get(
+          `http://localhost:8080/api/users/${userId}`
+        ); // 현재 사용자 정보 엔드포인트
         console.log("User data fetched successfully:", response.data);
         setUser(response.data);
         setLoading(false);
@@ -64,7 +67,10 @@ const EditUser: React.FC = () => {
     e.preventDefault();
     if (user) {
       try {
-        await axiosInstance.put(`http://localhost:8080/api/users/${user.id}`, user);
+        await axiosInstance.put(
+          `http://localhost:8080/api/users/${user.id}`,
+          user
+        );
         alert("Profile updated successfully");
         navigate("/mypage"); // 프로필 페이지로 이동
       } catch (error) {
@@ -77,26 +83,28 @@ const EditUser: React.FC = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div>
+    <div className="edit-user">
       <h2>Edit Profile</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Username: </label>
           <input
             type="text"
             name="username"
             value={user?.username}
             onChange={handleChange}
             readOnly
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Password: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Password: </label>
           <input
             type={showPassword ? "text" : "password"}
             name="password"
             onChange={handleChange}
             required
+            className="euInp"
           />
           {password && (
             <button type="button" onClick={toggleShowPassword}>
@@ -104,72 +112,79 @@ const EditUser: React.FC = () => {
             </button>
           )}
         </div>
-        <div>
-          <label>Email: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Email: </label>
           <input
             type="email"
             name="email"
             value={user?.email}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Nickname: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Nickname: </label>
           <input
             type="text"
             name="nickname"
             value={user?.nickname}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Age: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Age: </label>
           <input
             type="text"
             name="age"
             value={user?.age}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Phone Number: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Phone Number: </label>
           <input
             type="text"
             name="phoneNumber"
             value={user?.phoneNumber}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Address: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Address: </label>
           <input
             type="text"
             name="address"
             value={user?.address}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Gender: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Gender: </label>
           <input
             type="text"
             name="gender"
             value={user?.gender}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
-        <div>
-          <label>Birth Date: </label>
+        <div className="eu-input">
+          <label className="eu-inputl">Birth Date: </label>
           <input
             type="date"
             name="birthDate"
             value={user?.birthDate}
             onChange={handleChange}
+            className="euInp"
           />
         </div>
         <button type="submit">Update Profile</button>
       </form>
-      <div>
+      <div className="test-links">
         <a href="/diagnosisSkin">Skin Type Test</a>
         <a href="/diagnosisSclap">Scalp Type Test</a>
       </div>
