f7e46f0849092b41b431a9c864f93f333e0c727a
Jihyun O <wlgus0155@naver.com>
2024-06-07 15:53:16 +0900

    진단 결과 css 수정, 결제 페이지 제작

--- src/assets/Diagnosis/SclapResult.tsx
+++ src/assets/Diagnosis/SclapResult.tsx
@@ -2,97 +2,90 @@ import React from "react";
 import "./Result.css";
 
 const SclapResult: React.FC = () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const result = queryParams.get('result');
-    const letters = result ? result.split(", ") : [];
+  const queryParams = new URLSearchParams(window.location.search);
+  const result = queryParams.get("result");
+  const letters = result ? result.split(", ") : [];
 
-    // 각 결과값의 타입을 정의합니다.
-    const sclapTypes: { [key: string]: string } = {
-        "D": "건성 - Dry : 땀과 피지의 분비량은 적고 각질 형성이 빠른 타입",
-        "O": "지성 및 지루성 - Oily : 과도한 피지분비로 인해 모공이 막히기 쉽고 두피 모낭 속으로 피지가 쌓여 염증이나 트러블이 생기기 쉬운 타입",
-        "A": "탈모 진행 - Alopecia : 정상적으로 모발이 존재해야 할 부위에 모발이 없는 상태로 일반적으로 두피의 성모가 빠지는 상태",
-        "N": "정상 - Normal : 10~15%의 모낭이 퇴행기나 휴지기에 있어 하루 평균 50~60여 개 정도의 머리털이 빠지는 상태로 모발사이의 밀집도와 모발 굵기가 균일함",
-        "S": "두피 민감 - Sensitive : 두피의 염증이나 붉은기가 육안으로도 확인 가능하며 항상 두피가 부어있어 자극에 의한 손상이 쉬운 타입",
-        "R": "비민감 - Non-Sensitive : 유·수분이 고루 균형을 이루고 있으며 불순물이 없고 모공 주변이 깨끗한 상태",
-        "H": "손상 모발 - Hair damage : 잦은 시술 및 외부적 요인으로 인해 모발이 잘 엉키고 끊어지며 갈라지는 상태.",
-        "I": "비손상 - Intact : 모발이 굵고 강한 탄력이 있으며 큐티클이 촘촘하여 보이기에 윤기가 흐르는 상태",
-    };
+  const sclapTypes: { [key: string]: string } = {
+    D: "건성 - Dry : 땀과 피지의 분비량은 적고 각질 형성이 빠른 타입",
+    O: "지성 및 지루성 - Oily : 과도한 피지분비로 인해 모공이 막히기 쉽고 두피 모낭 속으로 피지가 쌓여 염증이나 트러블이 생기기 쉬운 타입",
+    A: "탈모 진행 - Alopecia : 정상적으로 모발이 존재해야 할 부위에 모발이 없는 상태로 일반적으로 두피의 성모가 빠지는 상태",
+    N: "정상 - Normal : 10~15%의 모낭이 퇴행기나 휴지기에 있어 하루 평균 50~60여 개 정도의 머리털이 빠지는 상태로 모발사이의 밀집도와 모발 굵기가 균일함",
+    S: "두피 민감 - Sensitive : 두피의 염증이나 붉은기가 육안으로도 확인 가능하며 항상 두피가 부어있어 자극에 의한 손상이 쉬운 타입",
+    R: "비민감 - Non-Sensitive : 유·수분이 고루 균형을 이루고 있으며 불순물이 없고 모공 주변이 깨끗한 상태",
+    H: "손상 모발 - Hair damage : 잦은 시술 및 외부적 요인으로 인해 모발이 잘 엉키고 끊어지며 갈라지는 상태.",
+    I: "비손상 - Intact : 모발이 굵고 강한 탄력이 있으며 큐티클이 촘촘하여 보이기에 윤기가 흐르는 상태",
+  };
 
-    // 각 타입별 추천 제품을 정의합니다. 같은 방법으로 추가 가능
-    const productRecommendations: { [key: string]: { name: string, image: string, link: string }[] } = {
-        "D": [
-            { name: "D 추천 : 샴푸1", image: "", link: "" },
-        ],
-        "O": [
-            { name: "O 추천 : 샴푸2", image: "", link: "" },
-        ],
-        "A": [
-            { name: "A 추천 : 샴푸1", image: "", link: "" },
-        ],
-        "N": [
-            { name: "N 추천 : 샴푸2", image: "", link: "" },
-        ],
-        "S": [
-            { name: "S 추천 : 샴푸1", image: "", link: "" },
-        ],
-        "R": [
-            { name: "R 추천 : 샴푸2", image: "", link: "" },
-        ],
-        "H": [
-            { name: "H 추천 : 샴푸1", image: "", link: "" },
-        ],
-        "I": [
-            { name: "I 추천 : 샴푸2", image: "", link: "" },
-        ],
-    };
+  const productRecommendations: {
+    [key: string]: { name: string; image: string; link: string }[];
+  } = {
+    D: [{ name: "D 추천 : 샴푸1", image: "", link: "" }],
+    O: [{ name: "O 추천 : 샴푸2", image: "", link: "" }],
+    A: [{ name: "A 추천 : 샴푸1", image: "", link: "" }],
+    N: [{ name: "N 추천 : 샴푸2", image: "", link: "" }],
+    S: [{ name: "S 추천 : 샴푸1", image: "", link: "" }],
+    R: [{ name: "R 추천 : 샴푸2", image: "", link: "" }],
+    H: [{ name: "H 추천 : 샴푸1", image: "", link: "" }],
+    I: [{ name: "I 추천 : 샴푸2", image: "", link: "" }],
+  };
 
+  const getsclapTypeDescriptions = (letters: string[]) => {
+    return letters.map((letter) => ({
+      letter,
+      description: sclapTypes[letter],
+    }));
+  };
 
-    // 결과 값에 대응하는 피부 타입을 가져오는 함수
-    const getsclapTypeDescriptions = (letters: string[]) => {
-        return letters.map(letter => ({
-            letter,
-            description: sclapTypes[letter]
-        }));
-    };
-
-    /// 추천 상품을 가져오는 함수
-    const getProductRecommendations = (letters: string[]) => {
-        return letters.reduce((acc: { name: string, image: string, link: string }[], letter) => {
-            if (productRecommendations[letter]) {
-                acc.push(...productRecommendations[letter]);
-            }
-            return acc;
-        }, []);
-    };
-
-    const sclapTypeDescriptions = getsclapTypeDescriptions(letters);
-    const recommendations = getProductRecommendations(letters);
-
-    return (
-        <div className="result-container">
-            <h1>두피 진단 결과</h1>
-            <h2> 회원님의 두피 MBTI 는 [ {letters.join("")} ] 입니다.</h2>
-            <div className="result-descriptions">
-                {sclapTypeDescriptions.map((sclapType) => (
-                    <div key={sclapType.letter}>
-                        <h3>[ {sclapType.letter} ]</h3>
-                        <p>▪ {sclapType.description}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="result-recommendations">
-                <h2>✨ {letters.join("")} 추천 제품 ✨</h2>
-                <div className="result-grid">
-                    {recommendations.map((product, index) => (
-                        <div key={index}>
-                            <img src={product.image} alt={product.name}/>
-                            <p>{product.name}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
+  const getProductRecommendations = (letters: string[]) => {
+    return letters.reduce(
+      (acc: { name: string; image: string; link: string }[], letter) => {
+        if (productRecommendations[letter]) {
+          acc.push(...productRecommendations[letter]);
+        }
+        return acc;
+      },
+      []
     );
-}
+  };
+
+  const sclapTypeDescriptions = getsclapTypeDescriptions(letters);
+  const recommendations = getProductRecommendations(letters);
+
+  return (
+    <div className="result-container">
+      <h1 className="result-title">두피 진단 결과</h1>
+      <div className="content-background">
+        <h2 className="result-name">
+          {" "}
+          회원님의 두피 MBTI 는{" "}
+          <span className="highlight">[ {letters.join("")} ]</span> 입니다.
+        </h2>
+        <div className="result-descriptions">
+          {sclapTypeDescriptions.map((sclapType) => (
+            <div key={sclapType.letter}>
+              <h3 className="highlight-h3">&lt; {sclapType.letter} &gt;</h3>
+              <p>▪ {sclapType.description}</p>
+            </div>
+          ))}
+        </div>
+        <div className="result-recommendations">
+          <h2 className="result-name">
+            {" "}
+            <span className="highlight">{letters.join("")}</span> 추천 제품{" "}
+          </h2>
+          <div className="result-grid">
+            {recommendations.map((product, index) => (
+              <div key={index}>
+                <img src={product.image} alt={product.name} />
+                <p>{product.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SclapResult;
--- src/assets/Diagnosis/SkinResult.tsx
+++ src/assets/Diagnosis/SkinResult.tsx
@@ -2,97 +2,94 @@ import React from "react";
 import "./Result.css";
 
 const SkinResult: React.FC = () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const result = queryParams.get('result');
-    const letters = result ? result.split(", ") : [];
+  const queryParams = new URLSearchParams(window.location.search);
+  const result = queryParams.get("result");
+  const letters = result ? result.split(", ") : [];
 
-    // 각 결과값의 타입을 정의합니다.
-    const skinTypes: { [key: string]: string } = {
-        "O": "지성 - Oily : 피지 분비량이 많아 번들거리고 여드름이 자주 생기는 타입",
-        "D": "건성 - Dry : 피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입",
-        "S": "민감성 - Sensitive : 피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입",
-        "R": "저항성 - Resistant : 피부 장벽이 견고해 외부적인 스트레스에 대해 견디는 힘이 강한 타입",
-        "P": "색소성 - Pigmented : 멜라닌 활성도가 높아 기미, 주근깨 혹은 잡티 등 눈에 보이는 색소가 많은 타입",
-        "N": "비색소성 - Non-Pigmented : 멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입",
-        "W": "주름성 - Wrinkled : 피부 결이 고르지 않고 주름이 많은 타입",
-        "T": "탄력성 - Tight : 피부 결이 고르고 주름이 적어 탄력이 있는 타입",
-    };
+  // 각 결과값의 타입을 정의합니다.
+  const skinTypes: { [key: string]: string } = {
+    O: "지성 - Oily : 피지 분비량이 많아 번들거리고 여드름이 자주 생기는 타입",
+    D: "건성 - Dry : 피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입",
+    S: "민감성 - Sensitive : 피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입",
+    R: "저항성 - Resistant : 피부 장벽이 견고해 외부적인 스트레스에 대해 견디는 힘이 강한 타입",
+    P: "색소성 - Pigmented : 멜라닌 활성도가 높아 기미, 주근깨 혹은 잡티 등 눈에 보이는 색소가 많은 타입",
+    N: "비색소성 - Non-Pigmented : 멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입",
+    W: "주름성 - Wrinkled : 피부 결이 고르지 않고 주름이 많은 타입",
+    T: "탄력성 - Tight : 피부 결이 고르고 주름이 적어 탄력이 있는 타입",
+  };
 
-    // 각 타입별 추천 제품을 정의합니다. 같은 방법으로 추가 가능
-    const productRecommendations: { [key: string]: { name: string, image: string, link: string }[] } = {
-        "O": [
-            { name: "O 추천 : 로션1", image: "", link: "" },
-        ],
-        "D": [
-            { name: "D 추천 : 로션2", image: "", link: "" },           
-        ],
-        "S": [
-            { name: "S 추천 : 로션3", image: "", link: "" },            
-        ],
-        "R": [
-            { name: "R 추천 : 로션4", image: "", link: "" },            
-        ],
-        "P": [
-            { name: "P 추천 : 로션5", image: "", link: "" },            
-        ],
-        "N": [
-            { name: "N 추천 : 로션6", image: "", link: "" },           
-        ],
-        "W": [
-            { name: "W 추천 : 로션7", image: "", link: "" },           
-        ],
-        "T": [
-            { name: "T 추천 : 로션8", image: "", link: "" },            
-        ],
-    };
+  // 각 타입별 추천 제품을 정의합니다. 같은 방법으로 추가 가능
+  const productRecommendations: {
+    [key: string]: { name: string; image: string; link: string }[];
+  } = {
+    O: [{ name: "O 추천 : 로션1", image: "", link: "" }],
+    D: [{ name: "D 추천 : 로션2", image: "", link: "" }],
+    S: [{ name: "S 추천 : 로션3", image: "", link: "" }],
+    R: [{ name: "R 추천 : 로션4", image: "", link: "" }],
+    P: [{ name: "P 추천 : 로션5", image: "", link: "" }],
+    N: [{ name: "N 추천 : 로션6", image: "", link: "" }],
+    W: [{ name: "W 추천 : 로션7", image: "", link: "" }],
+    T: [{ name: "T 추천 : 로션8", image: "", link: "" }],
+  };
 
+  // 결과 값에 대응하는 피부 타입을 가져오는 함수
+  const getSkinTypeDescriptions = (letters: string[]) => {
+    return letters.map((letter) => ({
+      letter,
+      description: skinTypes[letter],
+    }));
+  };
 
-    // 결과 값에 대응하는 피부 타입을 가져오는 함수
-    const getSkinTypeDescriptions = (letters: string[]) => {
-        return letters.map(letter => ({
-            letter,
-            description: skinTypes[letter]
-        }));
-    };
-
-    /// 추천 상품을 가져오는 함수
-    const getProductRecommendations = (letters: string[]) => {
-        return letters.reduce((acc: { name: string, image: string, link: string }[], letter) => {
-            if (productRecommendations[letter]) {
-                acc.push(...productRecommendations[letter]);
-            }
-            return acc;
-        }, []);
-    };
-
-    const skinTypeDescriptions = getSkinTypeDescriptions(letters);
-    const recommendations = getProductRecommendations(letters);
-
-    return (
-        <div className="result-container">
-            <h1>피부 진단 결과</h1>
-            <h2> 회원님의 피부 MBTI 는 [ {letters.join("")} ] 입니다.</h2>
-            <div className="result-descriptions">
-                {skinTypeDescriptions.map((skinType) => (
-                    <div key={skinType.letter}>
-                        <h3>[ {skinType.letter} ]</h3>
-                        <p>▪ {skinType.description}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="result-recommendations">
-                <h2>✨ {letters.join("")} 추천 제품 ✨</h2>
-                <div className="result-grid">
-                    {recommendations.map((product, index) => (
-                        <div key={index}>
-                            <img src={product.image} alt={product.name} />
-                            <p>{product.name}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
+  /// 추천 상품을 가져오는 함수
+  const getProductRecommendations = (letters: string[]) => {
+    return letters.reduce(
+      (acc: { name: string; image: string; link: string }[], letter) => {
+        if (productRecommendations[letter]) {
+          acc.push(...productRecommendations[letter]);
+        }
+        return acc;
+      },
+      []
     );
-}
+  };
+
+  const skinTypeDescriptions = getSkinTypeDescriptions(letters);
+  const recommendations = getProductRecommendations(letters);
+
+  return (
+    <div className="result-container">
+      <h1 className="result-title">피부 진단 결과</h1>
+      <div className="content-background">
+        <h2 className="result-name">
+          {" "}
+          회원님의 피부 MBTI 는{" "}
+          <span className="highlight">[ {letters.join("")} ]</span> 입니다.
+        </h2>
+        <div className="result-descriptions">
+          {skinTypeDescriptions.map((skinType) => (
+            <div key={skinType.letter}>
+              <h3 className="highlight-h3">&lt; {skinType.letter} &gt;</h3>
+              <p>▪ {skinType.description}</p>
+            </div>
+          ))}
+        </div>
+        <div className="result-recommendations">
+          <h2 className="result-name">
+            {" "}
+            <span className="highlight">{letters.join("")}</span> 추천 제품{" "}
+          </h2>
+          <div className="result-grid">
+            {recommendations.map((product, index) => (
+              <div key={index}>
+                <img src={product.image} alt={product.name} />
+                <p>{product.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SkinResult;
